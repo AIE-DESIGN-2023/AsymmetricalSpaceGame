c269de8e38014de4b0027ea3d44c4ab5c2107593
AIE\s214337 <s214337@students.aie.edu.au>
2024-05-14 16:44:25 +0930

    Added SHipAI and door script

--- Spaceship game prototype/Assets/Scripts/AIMechanicController.cs
+++ Spaceship game prototype/Assets/Scripts/AIMechanicController.cs
@@ -1,19 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class AIMechanicController : MonoBehaviour
 {
+    public GameObject doorAlpha;
+    public Transform doorAMovePoint1;
+    public Transform doorAMovepoint2;
+
+    public bool alphaDoorOnCooldown;
+
+    public float alphaDoorActiveDuration;
+    private float currentAlphaDoorDuration;
+    public float alphaDoorCooldownDuration;
+    public float alphaDoorTelegraphDuration;
+    [Space]
+    public float betaDoorActiveDuration;
+    private float currentBetaDoorActiveDuration;
+    public float betaDoorCooldownDuration;
+    public float abetaDoorTelegraphDuration;
+
+    public Image alphaDoorFillImage;
+
+
+    [Space]
+    private bool aDFadeIn;
+    private bool aDFadeOut;
+    private float aDTimeToFade = 5f;
+    public GameObject aDLoadingBarObject;
+    public Image aDLoadingBarImage;
+    public CanvasGroup aDLoadCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentAlphaDoorDuration = alphaDoorActiveDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (alphaDoorOnCooldown) //change image fills 
+        { alphaDoorFillImage.fillAmount = currentAlphaDoorDuration / alphaDoorCooldownDuration; }
+        else
+        { alphaDoorFillImage.fillAmount = currentAlphaDoorDuration / alphaDoorActiveDuration; }
+
+        if (currentAlphaDoorDuration <= 0) //if active timer runs out, start cooldown - timer only goes back up if hits 0 or below
+        { alphaDoorOnCooldown = true; currentAlphaDoorDuration += Time.deltaTime; }
+
+        if (currentAlphaDoorDuration >= alphaDoorCooldownDuration) //once cooldown ends set timer back to active timer
+        { alphaDoorOnCooldown = false; currentAlphaDoorDuration = alphaDoorActiveDuration; }
+
+
+
+        if (Input.GetKeyDown(KeyCode.L) && alphaDoorOnCooldown == false)
+        {
+            //do the doors
+
+
+            Debug.Log("activated door group alpha");
+        }
     }
+
+    /*public void ActivateDoorA(InputAction.CallbackContext context)
+    {
+        Debug.Log("womp womp");
+    }*/
+
+
 }
