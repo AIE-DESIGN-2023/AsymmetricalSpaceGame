9776ad991250586838a60dca95bca95421cf93b1
AIE\s214337 <s214337@students.aie.edu.au>
2024-06-03 16:53:24 +0930

    added game load screen

---
+++ Spaceship game prototype/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject characterObjectivesGroup;
+    public GameObject PlayerManager;
+    public GameObject blackImage;
+    [SerializeField] CanvasGroup characterObjectivesCanvasGroup;
+    [SerializeField] GameObject playersCanvasGroup;
+    bool fadeOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackImage.SetActive(true);
+        Invoke("FadeOuttaHere", 6f);
+        PlayerManager.SetActive(false);
+        playersCanvasGroup.SetActive(false);
+        characterObjectivesCanvasGroup.alpha = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (hackFadeIn)
+        {
+            if (hackLoadCanvas.alpha < 1)
+            {
+                hackLoadCanvas.alpha += hackTimeToFade * Time.deltaTime;
+                if (hackLoadCanvas.alpha >= 1)
+                {
+                    hackFadeIn = false;
+                }
+            }
+        }*/
+        if (fadeOut)
+        {
+            if (characterObjectivesCanvasGroup.alpha >= 0)
+            {
+                characterObjectivesCanvasGroup.alpha -= 2 * Time.deltaTime;
+                if (characterObjectivesCanvasGroup.alpha <= 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
+
+    }
+
+    void FadeOuttaHere()
+    {
+        fadeOut = true;
+        Invoke("ActivateOuttaHere", 1f);
+
+    }
+
+    void ActivateOuttaHere()
+    {
+        PlayerManager.SetActive(true);
+        playersCanvasGroup.SetActive(true);
+    }
+}
